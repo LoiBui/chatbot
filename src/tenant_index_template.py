86c4d0f6287f521d4ce442f500fd554f8e51cfd0
Bùi Công Lợi <hts.congloi@gmail.com>
2021-01-14 14:41:33 +0700

    add

--- src/tenant_index_template.py
+++ src/tenant_index_template.py
@@ -9,30 +9,25 @@ import sateraito_func
 from google.appengine.ext import blobstore
 
 _gnaviid = 'DASHBOARD'
-_leftmenuid = 'ADD'
+_leftmenuid = 'INDEX'
 class Page(TenantAppHelper):
 
   def processOfRequest(self, tenant):
 		try:
-
-			blobstore_url = sateraito_inc.my_site_url + '/tenant/blobstore/save'
-			url = blobstore.create_upload_url(blobstore_url)
-
 			ucfp = UcfTenantParameter(self)
 			ucfp.data['gnaviid'] = _gnaviid
 			ucfp.data['leftmenuid'] = _leftmenuid
 			ucfp.data['explains'] = [self.getMsg('EXPLAIN_OPERATIONLOG_HEADER')]
 
 			template_vals = {
-				'ucfp' : ucfp,
-				'url' : url
+				'ucfp' : ucfp
 			}
 			self.appendBasicInfoToTemplateVals(template_vals)
 
-			self.render('template_add.html', self._design_type, template_vals)
+			self.render('template_index.html', self._design_type, template_vals)
 		except BaseException, e:
 			self.outputErrorLog(e)
 			self.redirectError(UcfMessage.getMessage(self.getMsg('MSG_SYSTEM_ERROR'), ()))
 			return
 
-app = ndb.toplevel(webapp2.WSGIApplication([('/a/([^/]*)/add_template', Page)], debug=sateraito_inc.debug_mode, config=sateraito_func.wsgi_config))
+app = ndb.toplevel(webapp2.WSGIApplication([('/a/([^/]*)/template', Page)], debug=sateraito_inc.debug_mode, config=sateraito_func.wsgi_config))
