da1c706248b1fb4fc77299a54df7846eb8797fce
Trungnd <nguyenductrung@gmail.com>
2021-01-22 11:59:09 +0700

    update language

--- 1_closurecompiler.bat
+++ 1_closurecompiler.bat
@@ -1,2 +1,2 @@
-CALL /var/www/html/sateraito-fileup/UcfCallClosuerCompiler/ucf/bin20/common/UcfCallClosureCompiler.exe /var/www/html/sateraito-fileup/ExecuteConfig.xml
+CALL D:\loi\chatbot\UcfCallClosuerCompiler\ucf\bin20\common\UcfCallClosureCompiler.exe D:\loi\chatbot\ExecuteConfig.xml
 pause
